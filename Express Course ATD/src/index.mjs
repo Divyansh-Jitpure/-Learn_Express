f5acd8d52ef896bbe89705c0d81fed836e927278
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-07-30 00:42:35 +0530

    Query Params

--- Express Course ATD/src/index.mjs
+++ Express Course ATD/src/index.mjs
@@ -5,9 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
-  { id: 1, userName: "dj", displayName: "DJ" },
-  { id: 2, userName: "mj", displayName: "MJ" },
-  { id: 3, userName: "sj", displayName: "SJ" },
+  { id: 1, username: "divyansh", displayName: "DJ" },
+  { id: 2, username: "mj", displayName: "MJ" },
+  { id: 3, username: "sj", displayName: "SJ" },
+  { id: 4, username: "cj", displayName: "CJ" },
 ];
 
 app.get("/", (request, response) => {
@@ -15,7 +16,30 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-  response.send(mockUsers);
+  const {
+    query: { filter, value },
+  } = request;
+
+  if (!filter && !value) return response.send(mockUsers);
+
+  if (filter && value) {
+    const filteredUsers = mockUsers.filter((user) => {
+      if (user[filter]) {
+        return user[filter].includes(value);
+      }
+      return false;
+    });
+
+    if (filteredUsers.length === 0) {
+      return response.status(404).send({ msg: "No users found" });
+    }
+
+    return response.send(filteredUsers);
+  }
+
+  return response
+    .status(400)
+    .send({ msg: "Bad Request: Missing filter or value" });
 });
 
 app.get("/api/users/:id", (request, response) => {
