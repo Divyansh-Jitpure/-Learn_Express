4befb7d40facfd5a306f4cfa7d461e6251903eaf
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-08-14 22:07:11 +0530

    update

--- Express Course ATD/src/index.mjs
+++ Express Course ATD/src/index.mjs
@@ -1,20 +1,23 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
-  { id: 1, username: "divyansh", displayName: "DJ" },
-  { id: 2, username: "mj", displayName: "MJ" },
-  { id: 3, username: "sj", displayName: "SJ" },
-  { id: 4, username: "cj", displayName: "CJ" },
+  { id: 1, userName: "divyansh", displayName: "DJ" },
+  { id: 2, userName: "mj", displayName: "MJ" },
+  { id: 3, userName: "sj", displayName: "SJ" },
+  { id: 4, userName: "cj", displayName: "CJ" },
 ];
 
+// Route
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello" });
 });
 
+// Query Params
 app.get("/api/users", (request, response) => {
   const {
     query: { filter, value },
@@ -42,6 +45,15 @@ app.get("/api/users", (request, response) => {
     .send({ msg: "Bad Request: Missing filter or value" });
 });
 
+// Post request
+app.post("/api/users", (req, res) => {
+  const { body } = req;
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
+});
+
+// Route Params
 app.get("/api/users/:id", (request, response) => {
   console.log(request.params);
   const parsedId = parseInt(request.params.id);
@@ -52,8 +64,20 @@ app.get("/api/users/:id", (request, response) => {
   return response.send(findUser);
 });
 
+// sub route
 app.get("/api/products", (request, response) => {
   response.send([{ id: 123, name: "Quest 3", price: "54000" }]);
+});
+
+// Put request
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+  if (NaN(parsedId)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user);
 });
 
 app.listen(PORT, () => {
