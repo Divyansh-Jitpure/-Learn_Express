59b49152243e81d9cf453c1ecb88246a9586635e
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-09-24 19:41:25 +0530

    put request

--- Express Course ATD/src/index.mjs
+++ Express Course ATD/src/index.mjs
@@ -82,6 +82,24 @@ app.get("/api/quotes", (request, response) => {
 });
 
 // Put request
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return res.sendStatus(404);
+
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+
+  return res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
