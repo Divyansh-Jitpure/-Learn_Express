8d3b1d0b6b68865c9264785b3e068dd362596b53
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-09-30 18:10:58 +0530

    cookies

--- Express Course ATD/src/index.mjs
+++ Express Course ATD/src/index.mjs
@@ -2,9 +2,11 @@ import express from "express";
 import usersRouter from "./routes/users.mjs";
 import qotesAPIRouter from "./routes/quotesAPI.mjs";
 import productsRouter from "./routes/products.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser("secret"));
 app.use(usersRouter);
 app.use(qotesAPIRouter);
 app.use(productsRouter);
@@ -22,6 +24,7 @@ const PORT = process.env.PORT || 3000;
 // Get request
 // Route
 app.get("/", (request, response) => {
+  response.cookie("Hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello" });
 });
 
--- Express Course ATD/src/routes/products.mjs
+++ Express Course ATD/src/routes/products.mjs
@@ -4,7 +4,13 @@ const router = Router();
 
 // sub route
 router.get("/api/products", (request, response) => {
-  response.send([{ id: 123, name: "Quest 3", price: "54000" }]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies.Hello);
+  if (request.signedCookies.Hello && request.signedCookies.Hello === "world")
+    return response.send([{ id: 123, name: "Quest 3", price: "54000" }]);
+
+  return response.status(403).send({ msg: "Send the Correct Cookies" });
 });
 
 export default router;
